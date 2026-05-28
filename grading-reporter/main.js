b60f898d943ce29037204bc1fdc7e72c35b2b115
afsmaira <andre.smaira@gmail.com>
2026-05-28 00:03:52 -0300

    Enhance error handling for runner results processing

--- grading-reporter/main.js
+++ grading-reporter/main.js
@@ -7,11 +7,18 @@ try {
     .getInput("runners")
     .split(",")
     .map((runner) => {
-      const encodedResults = process.env[`${runner.trim().toUpperCase()}_RESULTS`];
-      const json = Buffer.from(encodedResults, "base64").toString("utf-8");
-      return { runner, results: JSON.parse(json) };
+      const rawResults = process.env[`${runner.trim().toUpperCase()}_RESULTS`] || "{}";
+      let jsonStr;
+      
+      try {
+        JSON.parse(rawResults);
+        jsonStr = rawResults;
+      } catch (e) {
+        jsonStr = Buffer.from(rawResults, "base64").toString("utf-8");
+      }
+      
+      return { runner, results: JSON.parse(jsonStr) };
     });
-
 
   ConsoleResults(runnerResults);
   NotifyClassroom(runnerResults);
@@ -23,12 +30,12 @@ try {
   }
 } catch (error) {
   const input = core.getInput("runners");
-  const pattern = /^([a-zA-Z0-9]+,)*[a-zA-Z0-9]+$/
+  const pattern = /^([a-zA-Z0-9]+,)*[a-zA-Z0-9]+$/;
   if (!pattern.test(input)) {
     console.error("The runners input must be a comma-separated list of strings.");
     core.setFailed("The runners input must be a comma-separated list of strings.");
   } else {
-    console.error(error.message)
+    console.error(error.message);
     core.setFailed(error.message);
   }
 }
